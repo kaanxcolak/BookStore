a19bcf55cc2b7d3fed18befff695a1e128ccc652
Kaan Çolak <kaancolak3458@gmail.com>
2023-08-21 10:56:29 +0300

    updated

---
+++ WebApi/Application/AuthorOperations/Command/CreateAuthor.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using WebApi.DBOperations;
+
+namespace WebApi.Application.AuthorOperations.Command
+{
+    public class CreateAuthor
+    {
+        private readonly BookStoreDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public CreateAuthorModel Model { get; set; }
+
+        public CreateAuthor(BookStoreDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+
+        }
+        
+    }
+    public class CreateAuthorModel
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
---
+++ WebApi/Controllers/AuthorController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.DBOperations;
+
+namespace WebApi.Controllers
+{
+    public class AuthorController:ControllerBase
+    {
+        private readonly BookStoreDbContext _dbContext;
+        private readonly IMapper _mapper;
+        
+        public AuthorController(BookStoreDbContext context, IMapper mapper)
+        {
+            _dbContext = context;
+            _mapper = mapper;
+        }
+    }
+}
