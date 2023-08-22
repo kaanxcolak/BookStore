8921ddd980d85bf69d3caa7510f4b0dd504445cf
Kaan Çolak <kaancolak3458@gmail.com>
2023-08-22 16:06:57 +0300

    updated

---
+++ WebApi/Application/AuthorOperations/Command/UpdateAuthor/UpdateAuthorValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace WebApi.Application.AuthorOperations.Command.UpdateAuthor
+{
+    public class UpdateAuthorValidator:AbstractValidator<UpdateAuthor>
+    {
+        public UpdateAuthorValidator()
+        {
+            RuleFor(x => x.Model.Name).MinimumLength(2).When(x => x.Model.Name != string.Empty);
+            RuleFor(x=>x.Model.LastName).MinimumLength(2).When(x=>x.Model.LastName != string.Empty);
+            RuleFor(x => x.Model.BookId).NotEmpty.GreaterThan(0);
+        }
+    }
+}
---
+++ WebApi/Application/AuthorOperations/Queries/GetAuthorDetailQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace WebApi.Application.AuthorOperations.Queries
+{
+    public class GetAuthorDetailQueryValidator:AbstractValidator<GetAuthorDetailQuery>  
+    {
+        public GetAuthorDetailQueryValidator()
+        {
+            RuleFor(x => x.AuthorId).GreaterThan(0);
+        }
+    }
+}
