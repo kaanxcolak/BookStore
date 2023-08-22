18a13f6e9ea28f0c6557a8b85f56e3a741d9a39e
Kaan Çolak <kaancolak3458@gmail.com>
2023-08-22 15:50:10 +0300

    create Validator

---
+++ WebApi/Application/AuthorOperations/Command/CreateAuthor/CreateAuthorValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace WebApi.Application.AuthorOperations.Command.CreateAuthor
+{
+    public class CreateAuthorValidator:AbstractValidator<CreateAuthor>
+    {
+        public CreateAuthorValidator()
+        {
+            RuleFor(x => x.Model.Name).MinimumLength(2).NotEmpty();
+            RuleFor(x => x.Model.LastName).MinimumLength(2).NotEmpty();
+            RuleFor(x=>x.Model.BookId).NotEmpty().GreaterThan(0);   
+        }
+    }
+}
