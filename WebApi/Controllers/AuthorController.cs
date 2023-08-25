9b9cd3a4273aa33778d8e77ea8f57c3be40493e7
Kaan Çolak <kaancolak3458@gmail.com>
2023-08-25 11:40:25 +0300

    updated

--- WebApi/Controllers/AuthorController.cs
+++ WebApi/Controllers/AuthorController.cs
@@ -1,18 +1,47 @@
 ﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Application.AuthorOperations.Queries;
 using WebApi.DBOperations;
 
 namespace WebApi.Controllers
 {
+    [Authorize]
+    [ApiController]
+    [Route("[controller]s")]
     public class AuthorController:ControllerBase
     {
-        private readonly BookStoreDbContext _dbContext;
+        private readonly IBookStoreDbContext _dbContext;
         private readonly IMapper _mapper;
         
-        public AuthorController(BookStoreDbContext context, IMapper mapper)
+        public AuthorController(IBookStoreDbContext context, IMapper mapper)
         {
             _dbContext = context;
             _mapper = mapper;
         }
+
+        [HttpGet]
+        public ActionResult GetAuthor()
+        {
+            GetAuthorQuery query = new GetAuthorQuery(_dbContext,_mapper);
+            var authors = query.Handle();
+            return Ok(authors);
+        }
+
+        [HttpGet("id")]
+        public ActionResult GetByIdAuthor(int id)
+        {
+            GetAuthorDetailQuery query = new GetAuthorDetailQuery(_dbContext,_mapper);
+            query.AuthorId = id;
+
+            GetAuthorDetailQueryValidator validator = new GetAuthorDetailQueryValidator();
+            validator.ValidateAndThrow(query);
+
+            var author = query.Handle();
+            return Ok(author);
+        }
+
+
     }
 }
