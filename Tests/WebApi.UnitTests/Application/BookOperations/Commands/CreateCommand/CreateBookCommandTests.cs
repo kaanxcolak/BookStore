fc87178a7f774245b3711e88f404881fdd3dd613
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-29 15:04:28 +0300

    CreateBookCommandTest

--- Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandTests.cs
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandTests.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApi.Application.BookOperations.Commands.CreateBook;
+using WebApi.DBOperations;
+using WebApi.Entities;
+using WebApi.UnitTests.TestSetup;
+using static WebApi.Application.BookOperations.Commands.CreateBook.CreateBookCommand;
 
 namespace WebApi.UnitTests.Application.BookOperations.Commands.CreateCommand
 {
-    public class CreateBookCommandTests
+    public class CreateBookCommandTests:IClassFixture<CommonTestFixture>
     {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreateBookCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;           
+        }
+
+        public void WhenAlreadyExistBookTitleIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            //arrange - hazırlık
+            var book = new Book() { Title = "Test_WhenAlreadyExistBookTitleIsGiven_InvalidOperationException_ShouldBeReturn", PageCount=100,PublishDate = new System.DateTime(1990,01,10), GenreId = 1}; 
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            CreateBookCommand command = new CreateBookCommand(_context,_mapper);
+            command.Model= new CreateBookModel()
+            {
+
+            }
+
+        }
+            //act - çalıştırma
+            //assert - doğrulama
+
+        }
     }
 }
