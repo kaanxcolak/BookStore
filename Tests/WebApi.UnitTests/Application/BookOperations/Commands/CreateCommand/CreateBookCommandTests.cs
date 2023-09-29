7fea6ed544f4c14c1e6ddf431714bfa9efa36485
Kaan Çolak <kaancolak3458@gmail.com>
2023-09-29 16:11:00 +0300

    updated

--- Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandTests.cs
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandTests.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ using static WebApi.Application.BookOperations.Commands.CreateBook.CreateBookCom
 
 namespace WebApi.UnitTests.Application.BookOperations.Commands.CreateCommand
 {
-    public class CreateBookCommandTests:IClassFixture<CommonTestFixture>
+    public class CreateBookCommandTests : IClassFixture<CommonTestFixture>
     {
         private readonly BookStoreDbContext _context;
         private readonly IMapper _mapper;
@@ -20,26 +21,25 @@ namespace WebApi.UnitTests.Application.BookOperations.Commands.CreateCommand
         public CreateBookCommandTests(CommonTestFixture testFixture)
         {
             _context = testFixture.Context;
-            _mapper = testFixture.Mapper;           
+            _mapper = testFixture.Mapper;
         }
 
+        [Fact]
         public void WhenAlreadyExistBookTitleIsGiven_InvalidOperationException_ShouldBeReturn()
         {
             //arrange - hazırlık
-            var book = new Book() { Title = "Test_WhenAlreadyExistBookTitleIsGiven_InvalidOperationException_ShouldBeReturn", PageCount=100,PublishDate = new System.DateTime(1990,01,10), GenreId = 1}; 
+            var book = new Book() { Title = "Test_WhenAlreadyExistBookTitleIsGiven_InvalidOperationException_ShouldBeReturn", PageCount = 100, PublishDate = new System.DateTime(1990, 01, 10), GenreId = 1 };
             _context.Books.Add(book);
             _context.SaveChanges();
 
-            CreateBookCommand command = new CreateBookCommand(_context,_mapper);
-            command.Model= new CreateBookModel()
-            {
+            CreateBookCommand command = new CreateBookCommand(_context, _mapper);
+            command.Model = new CreateBookModel() { Title = book.Title };
 
-            }
-
-        }
-            //act - çalıştırma
-            //assert - doğrulama
-
+            //act & assert - (Çalıştırma) & (Doğrulama)
+            FluentActions
+                .Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kitap Zaten Mevcut!");
         }
     }
 }
+
